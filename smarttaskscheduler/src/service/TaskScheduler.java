17b4802a1cc69fd3e1e108384b2db7748b836dbc
Vedika <vedika.0506@gmail.com>
2026-04-11 18:13:15 +0530

    i am updating

--- smarttaskscheduler/src/service/TaskScheduler.java
+++ smarttaskscheduler/src/service/TaskScheduler.java
@@ -1,5 +1,18 @@
 package service;
 
+import model.Task;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class TaskScheduler {
 
+    private PriorityBlockingQueue<Task> taskQueue = new PriorityBlockingQueue<>();
+
+    public void addTask(Task task) {
+        System.out.println("Task Added: " + task);
+        taskQueue.put(task);
+    }
+
+    public Task getTask() throws InterruptedException {
+        return taskQueue.take();
+    }
 }
